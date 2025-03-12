b0fbde3621e36cc00beae8396cd52f43b8f5be56
Marcus Alexio Prado <mgprado1@my.waketech.edu>
2025-03-12 13:55:31 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+let start = document.getElementById('startTimer');
+let stop = document.getElementById('stopTimer');
+let reset = document.getElementById('resetTimer');
+
+let timer;
+let pause;
+start.onclick = function(){
+    if(timer){
+        if (pause){
+            clock()
+            timer = setInterval(clock, 1000);
+            pause = false;
+        }else{
+            exit
+        }
+    } else {
+        clock()
+        timer = setInterval(clock, 1000);
+    }
+    
+}
+stop.onclick = function(){
+    clearInterval(timer)
+    pause = true;
+}
+reset.onclick = function(){
+    day.innerHTML = '00';
+    hour.innerHTML = '00';
+    second.innerHTML = '00';
+    clearInterval(timer)
+}
+
+let day = document.getElementById('day');
+let hour = document.getElementById('hour');
+let second = document.getElementById('second');
+
+function clock(){
+    let snum = Number(second.innerHTML);
+    let hnum = Number(hour.innerHTML);
+    let dnum = Number(day.innerHTML);
+    if (snum + 1 == 60){
+        second.innerHTML = '00';
+        if (hnum + 1 == 24){
+            hour.innerHTML = '00';
+            day.innerHTML = dnum + 1;
+        }else{
+            hour.innerHTML = hnum + 1;
+        }
+    }else{
+        second.innerHTML = snum + 1;
+    }
+}
